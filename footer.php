eaa8850ce21a5e03f732f583ab38fe15ba1fc3d1
Ulich Igor <ulichigor55@gmail.com>
2025-02-22 23:30:45 +0200

    файли для демонстрації

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
-// Выводит подвал сайта. Этот файл подключается в файле index.php и в других файлах шаблонов.
+<!-- Выводит подвал сайта. Этот файл подключается в файле index.php и в других файлах шаблонов. -->
 <footer class="footer">
-	<div class="container">
-		<!-- Ссылка на телефон, номер телефона берется из поля 'phone' в ACF -->
-		<a class="phone" href="tel:<?php the_field('phone') ?>"><?php the_field('phone') ?></a>
-		<!-- Электронная почта, берется из поля 'footer__email' в ACF -->
-		<div class="footer__email"><?php the_field('footer__email') ?></div>
-	</div>
+    <div class="container">
+        <!-- Ссылка на телефон, номер телефона берется из поля 'phone' в ACF -->
+        <a class="phone" href="tel:<?php the_field('phone') ?>"><?php the_field('phone') ?></a>
+        <!-- Электронная почта, берется из поля 'footer__email' в ACF -->
+        <div class="footer__email"><?php the_field('footer__email') ?></div>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 <!-- Подключение скриптов. Запускает хук wp_footer. Вызывается в подвале темы, в файле footer.php. -->
--- index.php
+++ index.php
@@ -1,125 +1,123 @@
-// Purpose: The main template file
-//
 <?php get_header(); ?>
 <!-- Подключает файл шаблона header.php (шапку сайта) -->
+
 <main class="main">
-	<div class="container">
-		<div class="main-title">
-			<?php the_field('main-title') ?>
-			<!-- Выводит значение пользовательского поля 'main-title' из ACF -->
-		</div>
-		<div class="main__text">
-			<?php the_field('main__text') ?>
-			<!-- Выводит значение пользовательского поля 'main__text' из ACF -->
-		</div>
-		<div class="main__img">
-			<img src="<?php bloginfo('template_url'); ?>/assets/images/opilcki-logo.png" alt="photo">
-			<!-- Выводит URL директории шаблона и добавляет путь к изображению -->
-		</div>
-		<div class="project">
-			<div class="project__title">
-				ПРОЕКТЫ ДОМОВ ИЗ БРУСА
-			</div>
-			<div class="project__item">
-				<div class="project__name">
-					ДОМ #1
-				</div>
-				<div class="project__size">
-					Размер дома: 6 x 6
-				</div>
-				<div class="project__area">
-					Общая площадь дома: 54 кв. м
-				</div>
-				<div class="project__price">
-					Стоимость - 402 000 рублей
-				</div>
-				<div class="project__images">
-					<div class="project__images-item">
-						<img src="<?php bloginfo('template_url'); ?>/assets/images/home1.jpg" alt="photo">
-						<!-- Выводит URL директории шаблона и добавляет путь к изображению -->
-					</div>
-					<div class="project__images-item">
-						<img src="<?php bloginfo('template_url'); ?>/assets/images/home1-drawing.png" alt="photo">
-						<!-- Выводит URL директории шаблона и добавляет путь к изображению -->
-					</div>
-				</div>
-			</div>
-			<div class="project__item">
-				<div class="project__name">
-					ДОМ #2
-				</div>
-				<div class="project__size">
-					Размер дома: 6 x 7,5
-				</div>
-				<div class="project__area">
-					Общая площадь дома: 72 кв. м
-				</div>
-				<div class="project__price">
-					Стоимость - 457 000 рублей
-				</div>
-				<div class="project__images">
-					<div class="project__images-item">
-						<img src="<?php bloginfo('template_url'); ?>/assets/images/home2.jpg" alt="photo">
-						<!-- Выводит URL директории шаблона и добавляет путь к изображению -->
-					</div>
-					<div class="project__images-item">
-						<img src="<?php bloginfo('template_url'); ?>/assets/images/home2-drawing.png" alt="photo">
-						<!-- Выводит URL директории шаблона и добавляет путь к изображению -->
-					</div>
-				</div>
-			</div>
-			<div class="project__item">
-				<div class="project__name">
-					ДОМ #3
-				</div>
-				<div class="project__size">
-					Размер дома: 8 х 7,5
-				</div>
-				<div class="project__area">
-					Общая площадь дома: 92 кв. м
-				</div>
-				<div class="project__price">
-					Стоимость - 635 000 рублей
-				</div>
-				<div class="project__images">
-					<div class="project__images-item">
-						<img src="<?php bloginfo('template_url'); ?>/assets/images/home3.jpg" alt="photo">
-						<!-- Выводит URL директории шаблона и добавляет путь к изображению -->
-					</div>
-					<div class="project__images-item">
-						<img src="<?php bloginfo('template_url'); ?>/assets/images/home3-drawing.png" alt="photo">
-						<!-- Выводит URL директории шаблона и добавляет путь к изображению -->
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="download">
-		<img src="<?php bloginfo('template_url'); ?>/assets/images/home-catalog.png" alt="photo">
-		<!-- Выводит URL директории шаблона и добавляет путь к изображению -->
-		<a href="#">СКАЧАТЬ ВЕСЬ КАТАЛОГ ДОМОВ</a>
-	</div>
-	<div class="container">
-		<div class="gallery">
-			<div class="gallery__title">
-				<?php the_field('gallery__title') ?>
-				<!-- Выводит значение пользовательского поля 'gallery__title'  -->
-			</div>
-			<div class="gallery__text">
-				<?php the_field('gallery__text') ?>
-				<!-- Выводит значение пользовательского поля 'gallery__text' -->
-			</div>
-			<div class="gallery__inner">
-				<?php the_field('gallery__inner') ?>
-				<!-- Выводит значение пользовательского поля 'gallery__inner' -->
-			</div>
-			<div class="main__img">
-				<img src="<?php bloginfo('template_url'); ?>/assets/images/opilcki-logo.png" alt="photo">
-				<!-- Выводит URL директории шаблона и добавляет путь к изображению -->
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="main-title">
+            <?php the_field('main-title') ?>
+            <!-- Выводит значение пользовательского поля 'main-title' из ACF -->
+        </div>
+        <div class="main__text">
+            <?php the_field('main__text') ?>
+            <!-- Выводит значение пользовательского поля 'main__text' из ACF -->
+        </div>
+        <div class="main__img">
+            <img src="<?php bloginfo('template_url'); ?>/assets/images/opilcki-logo.png" alt="photo">
+            <!-- Выводит URL директории шаблона и добавляет путь к изображению -->
+        </div>
+        <div class="project">
+            <div class="project__title">
+                ПРОЕКТЫ ДОМОВ ИЗ БРУСА
+            </div>
+            <div class="project__item">
+                <div class="project__name">
+                    ДОМ #1
+                </div>
+                <div class="project__size">
+                    Размер дома: 6 x 6
+                </div>
+                <div class="project__area">
+                    Общая площадь дома: 54 кв. м
+                </div>
+                <div class="project__price">
+                    Стоимость - 402 000 рублей
+                </div>
+                <div class="project__images">
+                    <div class="project__images-item">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/home1.jpg" alt="photo">
+                        <!-- Выводит URL директории шаблона и добавляет путь к изображению -->
+                    </div>
+                    <div class="project__images-item">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/home1-drawing.png" alt="photo">
+                        <!-- Выводит URL директории шаблона и добавляет путь к изображению -->
+                    </div>
+                </div>
+            </div>
+            <div class="project__item">
+                <div class="project__name">
+                    ДОМ #2
+                </div>
+                <div class="project__size">
+                    Размер дома: 6 x 7,5
+                </div>
+                <div class="project__area">
+                    Общая площадь дома: 72 кв. м
+                </div>
+                <div class="project__price">
+                    Стоимость - 457 000 рублей
+                </div>
+                <div class="project__images">
+                    <div class="project__images-item">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/home2.jpg" alt="photo">
+                        <!-- Выводит URL директории шаблона и добавляет путь к изображению -->
+                    </div>
+                    <div class="project__images-item">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/home2-drawing.png" alt="photo">
+                        <!-- Выводит URL директории шаблона и добавляет путь к изображению -->
+                    </div>
+                </div>
+            </div>
+            <div class="project__item">
+                <div class="project__name">
+                    ДОМ #3
+                </div>
+                <div class="project__size">
+                    Размер дома: 8 х 7,5
+                </div>
+                <div class="project__area">
+                    Общая площадь дома: 92 кв. м
+                </div>
+                <div class="project__price">
+                    Стоимость - 635 000 рублей
+                </div>
+                <div class="project__images">
+                    <div class="project__images-item">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/home3.jpg" alt="photo">
+                        <!-- Выводит URL директории шаблона и добавляет путь к изображению -->
+                    </div>
+                    <div class="project__images-item">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/home3-drawing.png" alt="photo">
+                        <!-- Выводит URL директории шаблона и добавляет путь к изображению -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="download">
+        <img src="<?php bloginfo('template_url'); ?>/assets/images/home-catalog.png" alt="photo">
+        <!-- Выводит URL директории шаблона и добавляет путь к изображению -->
+        <a href="#">СКАЧАТЬ ВЕСЬ КАТАЛОГ ДОМОВ</a>
+    </div>
+    <div class="container">
+        <div class="gallery">
+            <div class="gallery__title">
+                <?php the_field('gallery__title') ?>
+                <!-- Выводит значение пользовательского поля 'gallery__title' -->
+            </div>
+            <div class="gallery__text">
+                <?php the_field('gallery__text') ?>
+                <!-- Выводит значение пользовательского поля 'gallery__text' -->
+            </div>
+            <div class="gallery__inner">
+                <?php the_field('gallery__inner') ?>
+                <!-- Выводит значение пользовательского поля 'gallery__inner' -->
+            </div>
+            <div class="main__img">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/opilcki-logo.png" alt="photo">
+                <!-- Выводит URL директории шаблона и добавляет путь к изображению -->
+            </div>
+        </div>
+    </div>
 </main>
 <!-- Подключение футера сайта -->
 <?php get_footer(); ?>
-
