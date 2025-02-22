516128c07eb81bba9010a23dede0aac9aa600906
Ulich Igor <ulichigor55@gmail.com>
2025-02-22 06:31:35 +0200

    виправити для показу на GitHub повинно виглядати як професійна документація

--- footer.php
+++ footer.php
@@ -1,13 +1,10 @@
-	<footer class="footer">
-			<div class="container">
-			<a class="phone" href="tel:<?php the_field('phone') ?>"><?php the_field('phone') ?></a>
-			<div class="footer__email"><?php the_field('footer__email') ?></div>
-		</div>
-		</footer>
-```
-  <?php wp_footer(); ?>
-```
-  # подкючение скриптов апускает хук wp_footer — один из основных хуков, без которого не будут работать многие плагины. Вызывается в подвале темы, в файле footer.php. 
-	</body>
-
+<footer class="footer">
+	<div class="container">
+		<a class="phone" href="tel:<?php the_field('phone') ?>"><?php the_field('phone') ?></a>
+		<div class="footer__email"><?php the_field('footer__email') ?></div>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+<!-- Подключение скриптов. Запускает хук wp_footer — один из основных хуков, без которого не будут работать многие плагины. Вызывается в подвале темы, в файле footer.php. -->
+</body>
 </html>
